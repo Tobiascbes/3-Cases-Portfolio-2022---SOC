4f30096121109b0d502082267797dc465cbd7366
Tobiascbes <98935282+Tobiascbes@users.noreply.github.com>
2022-11-21 07:56:59 +0100

    Add files via upload

---
+++ Opgaver/3 cases - MainApp/Class2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case_3___Password
+{
+    public class Class2
+    {
+        public static void Fodbold()
+        {
+            start:
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Tast 1: Hvis der er blevet scoret et mål");
+                Console.WriteLine("Tast 2: For afleveringer");
+                Console.WriteLine("Tast 3: For afslut");
+                var switchkey = Console.ReadLine();
+                switch (switchkey)
+                {
+                    case "1":
+                        Console.WriteLine("Olé Olé Olé");
+                        goto start;
+
+                    case "2":
+                        Console.WriteLine("Hvor mange afleveringer er blevet spillet?");
+                        int afleveringer = Convert.ToInt32(Console.ReadLine());
+
+
+                        if (afleveringer >= 10)
+                        {
+                            Console.WriteLine("High Five – Jubel!!!");
+                            goto start;
+                        }
+                        if (afleveringer <= 1)
+                        {
+                            Console.WriteLine("Shh");
+                            goto start;
+                        }
+                        if (afleveringer > 1 | afleveringer <= 9)
+                        {
+                            while (afleveringer > 0)
+                            {
+                                Console.WriteLine("Huh!");
+                                afleveringer--;
+                            }
+                        }
+                        Console.ReadKey();
+                        goto start;
+                case "3":
+                    break;
+                }
+
+            
+        }
+        public static void Dans()
+        {
+            //Spørg og få vores variabler
+            string Navn1 = "";
+            string Navn2 = "";
+            Console.WriteLine("Hvad er danser 1 navn?");
+            Navn1 = Console.ReadLine();
+            Console.WriteLine("Hvor mange point fik " + Navn1 + " ?");
+            int point1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Hvor er danser 2 navn?");
+            Navn2 = Console.ReadLine();
+            Console.WriteLine("Hvor mange point fik " + Navn2 + " ?");
+            int point2 = Convert.ToInt32(Console.ReadLine());
+
+            //Læg variabler sammen og udskriv
+            Console.WriteLine(Navn1 + " & " + Navn2 + ": " + Case_2.AddPoints(point1, point2));
+
+            Console.ReadKey();
+        }
+        public class Case_2
+        {
+            public int Point = Convert.ToInt32(Console.ReadLine());
+
+            public static int AddPoints(int point1, int point2)
+            {
+
+                return point1 + point2;
+            }
+
+        }
+    }
+}
---
+++ Opgaver/3 cases - MainApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Case_3___Password;
+
+namespace Case_3
+{
+    internal class Password
+    {
+        static void Main(string[] args)
+        {
+        home:;
+            Console.WriteLine("Password is required for access");
+            string login = @"C:\Users\Tobcas\Desktop\Opgaver\3 cases - MainApp\Password.txt";
+            string passfile = @"C:\Users\Tobcas\Desktop\Opgaver\3 Cases\Case 3 - Password\Passwords og login\Password.txt";
+            string userpw = Console.ReadLine();
+        
+            //Check hvis passwords stemmer overens
+            if (File.ReadAllText(login).Contains(userpw))
+            {
+                Console.WriteLine("Correct password");
+                Console.ReadKey();
+         
+                Console.Clear();
+                Console.WriteLine("choose next option");
+                Console.WriteLine("N: Next \t C: Change password");
+                ConsoleKeyInfo input = Console.ReadKey();
+                switch (input.Key)
+                {
+                    case ConsoleKey.N:
+                        Console.Clear();
+                    start:
+                        Console.WriteLine("Hvilket program vil du bruge?");
+                        Console.WriteLine("F: Fodbold \t D: Dans");
+                        ConsoleKeyInfo input1 = Console.ReadKey();
+                        switch (input1.Key)
+                        {
+                            case ConsoleKey.F:
+                                Class2.Fodbold();
+                                goto start;
+                                break;
+
+                            case ConsoleKey.D:
+                                Console.Clear();
+                                Class2.Dans();
+                                goto start;
+                                break;
+                        }
+                     
+                        break;
+
+                        case ConsoleKey.C:
+                        Console.WriteLine("Type new password.");
+                        string newpass = Console.ReadLine();
+
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password try again");
+                goto home;
+            }
+
+   
+
+
+            Console.ReadKey();
+        }
+    }
+}
